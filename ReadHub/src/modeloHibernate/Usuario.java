595c87b997206ec4ca5d5b0c6f6c45a8f4de99fa
Ikasle Arangoya <ikasle@arangoya.local>
2025-01-17 12:24:23 +0100

    constanza

--- ReadHub/src/modeloHibernate/Usuario.java
+++ ReadHub/src/modeloHibernate/Usuario.java
@@ -37,7 +37,91 @@ public class Usuario {
         administrador, usuario
     }
 
-    // Constructors, getters, and setters
-    // ...
+    // Default constructor
+    public Usuario(String nombre2, String apellidos2, String contrasena2, String email2, int dni2, int telefono2, Rol usuario) {}
+
+	public Usuario(Long idUsuario, String dni, String nombre, String apellidos, String email, int telefono, Rol rol,
+			String contrasena) {
+		super();
+		this.idUsuario = idUsuario;
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.telefono = telefono;
+		this.rol = rol;
+		this.contrasena = contrasena;
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [idUsuario=" + idUsuario + ", dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos
+				+ ", email=" + email + ", telefono=" + telefono + ", rol=" + rol + ", contrasena=" + contrasena + "]";
+	}
+
 }
 
